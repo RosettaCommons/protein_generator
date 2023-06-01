b678056b8870546c7c1df8422a3ca082379eb3c9
Jacob Gershon <jgershon@digs.ipd.uw.edu>
2023-06-01 08:36:30 -0700

    added setup file (testing)

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='protein_generator',
+      version='1.1.0',
+      description='generate new proteins.',
+      author='Rosetta Commons',
+      url='https://github.com/RosettaCommons/protein_generator',
+      packages=find_packages(),)
